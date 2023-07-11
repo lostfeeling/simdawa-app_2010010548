0d94f2f241c11aafd4eaffbd51434ff9fa928050
losthisfeeling <fauzidrv1@gmail.com>
2023-07-11 18:50:13 +0800

    Membuat halaman pendaftaran, login dan pembatasan akses

--- beasiswa-app.sql
+++ beasiswa-app.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 06, 2023 at 08:16 PM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.2
+-- Generation Time: Jul 11, 2023 at 12:47 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -33,19 +33,14 @@ CREATE TABLE `beasiswa` (
   `tanggal_mulai` date DEFAULT NULL,
   `tanggal_selesai` date DEFAULT NULL,
   `jenis_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `beasiswa`
 --
 
 INSERT INTO `beasiswa` (`id`, `nama_beasiswa`, `tanggal_mulai`, `tanggal_selesai`, `jenis_id`) VALUES
-(1, 'Beasiswa Bidikmisi', '2023-07-16', '2023-12-08', 3),
-(2, 'Beasiswa olahraga', '2023-06-08', '2023-12-28', 5),
-(3, 'besiswa uniska', '2023-01-08', '2023-06-24', 3),
-(4, 'Beasiswa Uniska', '2023-06-13', '2023-12-29', 7),
-(5, 'Beasiswa Uniska', '2023-01-06', '2023-07-06', 8),
-(6, 'Beasiswa Swasta', '2023-02-14', '2023-07-22', 9);
+(1, 'Beasiswa BCA', '2023-07-01', '2023-07-31', 3);
 
 -- --------------------------------------------------------
 
@@ -57,16 +52,14 @@ CREATE TABLE `jenis_beasiswa` (
   `id` int(11) NOT NULL,
   `nama_jenis` varchar(50) DEFAULT NULL,
   `keterangan` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `jenis_beasiswa`
 --
 
 INSERT INTO `jenis_beasiswa` (`id`, `nama_jenis`, `keterangan`) VALUES
-(5, 'Beasiswa Tahfidz', 'Untuk mahasiswa yang hafal Al-Qur\'an minimal 5 Juz'),
-(8, 'KIP Kuliah', 'Beasiswa KIP Kuliah UNISKA'),
-(9, 'Beasiswa BCA Finance', 'Jenis beasiswa yang terdapat di perguruan tinggi adalah beasiswa BCA Finance');
+(3, 'BCA', 'Beasiswa BCA');
 
 -- --------------------------------------------------------
 
@@ -79,17 +72,17 @@ CREATE TABLE `pendaftaran_pengguna` (
   `no_pendaftaran` varchar(20) DEFAULT NULL,
   `nama_lengkap` varchar(75) DEFAULT NULL,
   `no_handphone` varchar(15) DEFAULT NULL,
-  `bukti_daftar` varchar(225) DEFAULT NULL,
+  `bukti_daftar` varchar(255) DEFAULT NULL,
   `prodi_id` int(11) DEFAULT NULL,
   `keterangan` enum('Sudah Diverifikasi','Belum Diverifikasi','Akun Dibatalkan') DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `pendaftaran_pengguna`
 --
 
 INSERT INTO `pendaftaran_pengguna` (`id`, `no_pendaftaran`, `nama_lengkap`, `no_handphone`, `bukti_daftar`, `prodi_id`, `keterangan`) VALUES
-(1, '1', 'Shellania', '087829508584', 'uniska_logo.png', NULL, 'Sudah Diverifikasi');
+(2, '2010010548', 'MUHAMMAD FAUZI', '0895365462060', 'uniska_logo1.png', NULL, 'Sudah Diverifikasi');
 
 -- --------------------------------------------------------
 
@@ -103,7 +96,7 @@ CREATE TABLE `pengajuan_beasiswa` (
   `pengguna_id` int(11) DEFAULT NULL,
   `beasiswa_id` int(11) DEFAULT NULL,
   `keterangan` enum('Diajukan','Ditolak','Diusulkan','Ditetapkan') DEFAULT 'Diajukan'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -114,19 +107,18 @@ CREATE TABLE `pengajuan_beasiswa` (
 CREATE TABLE `pengguna` (
   `id` int(11) NOT NULL,
   `username` varchar(50) DEFAULT NULL,
-  `PASSWORD` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
   `peran` enum('ADMIN','USER','OPERATOR') DEFAULT NULL,
   `login_terakhir` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `pendaftaran_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `pengguna`
 --
 
-INSERT INTO `pengguna` (`id`, `username`, `PASSWORD`, `peran`, `login_terakhir`, `pendaftaran_id`) VALUES
-(1, '1', '55ad0d2a271aa5e1ec11fdc60190b349', 'USER', '2023-06-24 02:32:04', 1),
-(2, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'ADMIN', '2023-06-24 03:16:29', NULL);
+INSERT INTO `pengguna` (`id`, `username`, `password`, `peran`, `login_terakhir`, `pendaftaran_id`) VALUES
+(2, '2010010548', '0192023a7bbd73250516f069df18b500', 'USER', '2023-07-10 02:39:50', 2);
 
 -- --------------------------------------------------------
 
@@ -138,16 +130,14 @@ CREATE TABLE `persyaratan` (
   `id` int(11) NOT NULL,
   `nama_persyaratan` varchar(100) DEFAULT NULL,
   `keterangan` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `persyaratan`
 --
 
 INSERT INTO `persyaratan` (`id`, `nama_persyaratan`, `keterangan`) VALUES
-(2, 'Warga Negara Indonesia', 'calon beasiswa peserta harus merupakan warga asli negara indonesia'),
-(3, 'Berusia maksimal 19 tahun', 'penerima beasiswa minimal berusia 19 tahun sebelum bulan agustus'),
-(4, 'IPK ', 'Minimal IPK 3,0');
+(1, 'SKCK', 'fotocopy skck.');
 
 -- --------------------------------------------------------
 
@@ -159,7 +149,7 @@ CREATE TABLE `persyaratan_beasiswa` (
   `id` int(11) NOT NULL,
   `beasiswa_id` int(11) DEFAULT NULL,
   `persyaratan_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -174,7 +164,7 @@ CREATE TABLE `persyaratan_pengajuan` (
   `tanggal_upload` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `nama_file` varchar(255) DEFAULT NULL,
   `keterangan` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -184,16 +174,16 @@ CREATE TABLE `persyaratan_pengajuan` (
 
 CREATE TABLE `prodi` (
   `id` int(11) NOT NULL,
-  `nama_prodi` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `nama_prodi` varchar(100) DEFAULT NULL,
+  `keterangan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `prodi`
 --
 
-INSERT INTO `prodi` (`id`, `nama_prodi`) VALUES
-(1, 'Teknik Informatika'),
-(4, 'Pendidikan Ekonomi');
+INSERT INTO `prodi` (`id`, `nama_prodi`, `keterangan`) VALUES
+(3, 'Teknik Sipil', 'Prodi Teknik Sipil Uniska');
 
 --
 -- Indexes for dumped tables
@@ -261,19 +251,19 @@ ALTER TABLE `prodi`
 -- AUTO_INCREMENT for table `beasiswa`
 --
 ALTER TABLE `beasiswa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `jenis_beasiswa`
 --
 ALTER TABLE `jenis_beasiswa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `pendaftaran_pengguna`
 --
 ALTER TABLE `pendaftaran_pengguna`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `pengguna`
@@ -285,7 +275,7 @@ ALTER TABLE `pengguna`
 -- AUTO_INCREMENT for table `persyaratan`
 --
 ALTER TABLE `persyaratan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `persyaratan_beasiswa`
@@ -303,7 +293,7 @@ ALTER TABLE `persyaratan_pengajuan`
 -- AUTO_INCREMENT for table `prodi`
 --
 ALTER TABLE `prodi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
