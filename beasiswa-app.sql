0e903cc79a539f3a6a7f07ae6ba094d1fb17846c
lostfeeling <84653868+lostfeeling@users.noreply.github.com>
2023-07-11 19:09:24 +0800

    Membuat halaman pendaftaran, login, dan pembatasan akses

--- beasiswa-app.sql
+++ beasiswa-app.sql
@@ -4,7 +4,7 @@
 --
 -- Host: 127.0.0.1
 -- Generation Time: Jul 11, 2023 at 01:01 PM
--- Server version: 10.4.27-MariaDB
+-- Server version: 10.4.27-MariaDB 
 -- PHP Version: 8.1.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
